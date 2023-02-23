2a1f9656641c99e237ebfcc1fc8463f0f87eeac0
Isaac True <isaac.true@canonical.com>
2023-02-23 13:27:15 +0100

    nemos-images-embedded-lunar: run_x86: update example run script
    The script to show an example of running the generated VM images should
    be in the directory corresponding to the image it is running.
    
    Additionally, update the command line to make it more compatible with
    systems that don't provide the `qemu-kvm` command.
    
    LP: #2008236
    Signed-off-by: Isaac True <isaac.true@canonical.com>

---
+++ nemos-images-embedded-lunar/run_x86.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if [ -d /var/tmp/my_lunar ];then
+    # target-dir used in README.rst
+    pushd /var/tmp/my_lunar
+fi
+
+qemu-system-x86_64 \
+    -m 1G \
+    -nographic \
+    -netdev user,id=user0,hostfwd=tcp::10022-:22 \
+    -device virtio-net-pci,netdev=user0 \
+    -drive file=nemos-image-embedded-lunar.x86_64-1.0.1.qcow2,if=virtio

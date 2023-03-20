af6a9ab6473e22b0a0d589025fe4f03dd6a0c420
Isaac True <isaac.true@canonical.com>
2023-03-20 10:57:42 +0100

    nemos-images-reference-lunar: x86, aarch64: add UEFI variables file
    Add an additional transient pflash storage for the UEFI variables.
    
    Additionally, mark the OVMF/AAVMF UEFI firmware as "readonly" so that the VM
    can be run without root permissions.
    
    Signed-off-by: Isaac True <isaac.true@canonical.com>

--- nemos-images-reference-lunar/run_aarch64.sh
+++ nemos-images-reference-lunar/run_aarch64.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 # SPDX-License-Identifier: GPL-2.0-or-later
 
+NVRAM=$(mktemp)
+cp /usr/share/AAVMF/AAVMF_VARS.fd ${NVRAM}
+
 qemu-system-aarch64 \
     -m 1G \
     --smp 2 \
     --cpu cortex-a53 \
     -nographic \
     -M virt \
-    -pflash /usr/share/AAVMF/AAVMF_CODE.fd \
+    -drive file="/usr/share/AAVMF/AAVMF_CODE.fd",readonly=on,if=pflash,format=raw,unit=0 \
+    -drive file="${NVRAM}",if=pflash,format=raw,unit=1 \
     -netdev user,id=user0,hostfwd=tcp::10022-:22 \
     -device virtio-net-pci,netdev=user0 \
     -drive file=nemos-image-reference-lunar.aarch64-1.0.1.qcow2,if=virtio
 
+rm ${NVRAM}

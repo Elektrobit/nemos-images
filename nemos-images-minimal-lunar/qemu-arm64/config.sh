a418094c95d40832f858d4d335d4597bb228a368
Isaac True <isaac.true@canonical.com>
2023-06-14 09:21:03 +0200

    ERL-211: nemos-images-*-lunar: qemu-*: add NemOS information to os-release
    Signed-off-by: Isaac True <isaac.true@canonical.com>

--- nemos-images-minimal-lunar/qemu-arm64/config.sh
+++ nemos-images-minimal-lunar/qemu-arm64/config.sh
@@ -5,6 +5,7 @@
 # Functions...
 #--------------------------------------
 test -f /.kconfig && . /.kconfig
+test -f /.profile && . /.profile
 
 set -ex
 
@@ -36,3 +37,14 @@ find /usr/share/doc/ ! -iname copyright -delete 2> /dev/null || true
 # Activate services
 #--------------------------------------
 baseInsertService systemd-networkd
+
+#=======================================
+# Update OS information
+#---------------------------------------
+echo "VARIANT=\"NemOS\"" >> /usr/lib/os-release
+echo "VARIANT_ID=\"nemos\"" >> /usr/lib/os-release
+echo "IMAGE_ID=\"${kiwi_iname}\"" >> /usr/lib/os-release
+echo "IMAGE_VERSION=\"${kiwi_iversion}\"" >> /usr/lib/os-release
+echo "BUILD_ID=\"$(date -I -u)\"" >> /usr/lib/os-release
+echo "NEMOS_HOME_URL=\"https://launchpad.net/nemos\"" >> /usr/lib/os-release
+echo "NEMOS_BUG_REPORT_URL=\"https://bugs.launchpad.net/nemos\"" >> /usr/lib/os-release
--- nemos-images-reference-lunar/qemu-amd64/config.sh
+++ nemos-images-reference-lunar/qemu-amd64/config.sh
@@ -79,3 +79,14 @@ EOF
             model=generic-classic > /var/lib/snapd/seed/assertions/model
     fi
 done
+
+#=======================================
+# Update OS information
+#---------------------------------------
+echo "VARIANT=\"NemOS\"" >> /usr/lib/os-release
+echo "VARIANT_ID=\"nemos\"" >> /usr/lib/os-release
+echo "IMAGE_ID=\"${kiwi_iname}\"" >> /usr/lib/os-release
+echo "IMAGE_VERSION=\"${kiwi_iversion}\"" >> /usr/lib/os-release
+echo "BUILD_ID=\"$(date -I -u)\"" >> /usr/lib/os-release
+echo "NEMOS_HOME_URL=\"https://launchpad.net/nemos\"" >> /usr/lib/os-release
+echo "NEMOS_BUG_REPORT_URL=\"https://bugs.launchpad.net/nemos\"" >> /usr/lib/os-release
